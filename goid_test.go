329af3046b24800f9c4a94d3ea04d20b4d16d14c
Oliver Martin <oliver@volatilevoid.net>
2017-02-10 20:46:39 +0100

    Add benchmark and sanity test

--- goid_test.go
+++ goid_test.go
@@ -1,10 +1,13 @@
 package goid
 
 import (
+	"reflect"
 	"runtime"
 	"strconv"
 	"strings"
 	"testing"
+
+	"github.com/leromarinvit/typist"
 )
 
 func TestGoid(t *testing.T) {
@@ -17,8 +20,7 @@ func compareGoid(count int, t *testing.T, done chan<- int) {
 	goid := Goid()
 	stack := goidFromStack()
 	if goid != stack {
-		t.Errorf("Goid() returned %d, stack trace said %d\n", goid, stack)
-		t.Fail()
+		t.Fatalf("Goid() returned %d, stack trace said %d\n", goid, stack)
 	}
 	if count == 0 {
 		done <- 1
@@ -34,4 +36,35 @@ func goidFromStack() (id int64) {
 	num = num[:strings.IndexByte(num, ' ')]
 	id, _ = strconv.ParseInt(num, 10, 64)
 	return id
+}
+
+func BenchmarkGoid(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Goid()
+	}
+}
+
+func TestRuntimeG(t *testing.T) {
+	pg, err := typist.TypeByString("*runtime.g")
+	if err != nil {
+		t.Fatal("*runtime.g not found")
+	}
+	if pg.Kind() != reflect.Ptr {
+		t.Fatal("*runtime.g isn't reflect.Ptr but", pg.Kind())
+	}
+	g := pg.Elem()
+	if g.Kind() != reflect.Struct {
+		t.Fatal("runtime.g isn't reflect.Struct but", g.Kind())
+	}
+	goid, ok := g.FieldByName("goid")
+	if !ok {
+		t.Fatal("runtime.g has no goid field")
+	}
+	if goid.Type.Kind() != reflect.Int64 {
+		t.Fatal("runtime.g.goid isn't int64 but", goid.Type.String())
+	}
+	_, ok = g.FieldByName("sched")
+	if !ok {
+		t.Fatal("runtime.g.sched has no sched field")
+	}
 }
